996ffb9a43babbf76a5236d394ce05cc12ad9b1b
EngIcaro <icarogabriel7@gmail.com>
2018-07-17 23:47:40 -0300

    add

---
+++ client.h
@@ -0,0 +1,17 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QObject>
+
+class client : public QObject
+{
+    Q_OBJECT
+public:
+    explicit client(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // CLIENT_H
